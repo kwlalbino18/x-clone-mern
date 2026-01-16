347f100ff2ff3ddb8f5fbc5d282d353d1c5e1de1
kwlalbino18 <kwlalbino18@gmail.com>
2026-01-16 18:02:21 -0400

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+//vamos a crear el servidor con express
+
+//en esta seccion importamos
+import express from "express"
+
+
+
+
+
+//creamos la funcion express
+const app = express();
+
+app.listen(5001, ()=> console.log("Server running on port 5001"))
+
+
