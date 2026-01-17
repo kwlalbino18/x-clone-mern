d4ae193670a1f36d13bf7733647a5224a35b14f9
kwlalbino18 <kwlalbino18@gmail.com>
2026-01-17 17:01:13 -0400

    startserver

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,9 +21,28 @@ const app = express();
 //conexion a la base de datos
 //solucionar problema de dns en windows
 dns.setServers(["1.1.1.1"]);
-connectDB();
-app.get("/", (req,res) => res.send("API is running..."))
+
+app.get("/", (req,res) => res.send("Hello from server"))
 
 app.listen(ENV.PORT, ()=> console.log("Server running on port:", ENV.PORT))
+
+const startServer =async () =>{
+try{
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server running on port:", ENV.PORT));
+}
+
+catch(error){
+    console.error("Failed to start server", error.message);
+    process.exit(1);
+
+
+}
+startServer();
+
+
+
+}
+
 
 
