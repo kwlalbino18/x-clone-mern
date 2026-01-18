ba848780094432810dbee6fc485e7b6ad9140aa8
kwlalbino18 <kwlalbino18@gmail.com>
2026-01-17 21:54:18 -0400

    feat: Implement initial backend setup including server, user authentication, profile management, and social features.

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import {
+    followUser,
+    getCurrentUser,
+    getUserProfile,
+    syncUser,
+    updateProfile,
+} from '../controllers/user.controller.js';
+
+import { protectRoute } from '../middleware/auth.middleware.js';
+
+const router = express.Router();
+
+//public routes
+router.get("/profile/:username", getUserProfile);
+
+//protected routes
+router.post("/sync", protectRoute, syncUser);
+router.post("/me", protectRoute, getCurrentUser);
+router.put("/profile", protectRoute, updateProfile);
+router.post("/follow/:targetUserId", protectRoute, followUser);
+//router.post("/unfollow/:targetUserId", protectRoute, unfollowUser);
+//update profile 
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,25 +4,27 @@
 import express from "express"
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/express"
+import userRoutes from "./routes/user.route.js"
 
 //este import es para solucionar un problema de dns en windows
 import dns from "node:dns/promises";
 
-
-
-
-
-
-
-
 //creamos la funcion express
 const app = express();
+app.use(cors())
+app.use(express.json())
+
+app.use(clerkMiddleware());
 
 //conexion a la base de datos
 //solucionar problema de dns en windows
 dns.setServers(["1.1.1.1"]);
 
 app.get("/", (req,res) => res.send("Hello from server"))
+
+app.use("/api/users", userRoutes)
 
 app.listen(ENV.PORT, ()=> console.log("Server running on port:", ENV.PORT))
 
